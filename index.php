58d76de480b3ccaee1762da3d2218800726c6159
alx rico <alexrico403@gmail.com>
2015-04-29 09:05:26 -0700

    2

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<script type="text/javascript" language="javascript" src="main.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </head>
 <body>
-<div class="dan">
-	<button type="button">Click Me!</button>
+<div id="dan">
+
+	<script type="text/javascript">
+function AlertIt() {
+var answer = confirm ("Please click on OK to continue.")
+if (answer)
+window.location="http://www.continue.com";
+}
+</script>
+
+<a href="javascript:AlertIt();">click me</a>
+
 </div>
-<div class="iwao">
-	<button type="button">Click Me!</button>
+<div id="iwao">
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
+
+</script>
+
+<a href="javascript:hide();">click me to hide</a>
 
 </div>
 </body>
